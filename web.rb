af2b549824715efcb879ec80bd7fc25c763ce567
chrsmys <chris@nschris.com>
2024-03-16 08:26:00 -0400

    Remove production checks.

--- web.rb
+++ web.rb
@@ -43,9 +43,6 @@ def validateApiKey
   if Stripe.api_key.start_with?('pk')
     return "Error: you used a publishable key to set up the example backend. Please use your test mode secret key. For more information, see https://stripe.com/docs/keys"
   end
-  if Stripe.api_key.start_with?('sk_live')
-    return "Error: you used a live mode secret key to set up the example backend. Please use your test mode secret key. For more information, see https://stripe.com/docs/keys#test-live-modes"
-  end
   return nil
 end
 
@@ -172,88 +169,6 @@ post '/cancel_payment_intent' do
   # Optionally reconcile the PaymentIntent with your internal order system.
   status 200
   return {:intent => payment_intent.id, :secret => payment_intent.client_secret}.to_json
-end
-
-# This endpoint creates a SetupIntent.
-# https://stripe.com/docs/api/setup_intents/create
-post '/create_setup_intent' do
-  validationError = validateApiKey
-  if !validationError.nil?
-    status 400
-    return log_info(validationError)
-  end
-
-  begin
-    setup_intent_params = {
-      :payment_method_types => params[:payment_method_types] || ['card_present'],
-    }
-
-    if !params[:customer].nil?
-      setup_intent_params[:customer] = params[:customer]
-    end
-
-    if !params[:description].nil?
-      setup_intent_params[:description] = params[:description]
-    end
-
-    if !params[:on_behalf_of].nil?
-      setup_intent_params[:on_behalf_of] = params[:on_behalf_of]
-    end
-
-    setup_intent = Stripe::SetupIntent.create(setup_intent_params)
-
-  rescue Stripe::StripeError => e
-    status 402
-    return log_info("Error creating SetupIntent! #{e.message}")
-  end
-
-  log_info("SetupIntent successfully created: #{setup_intent.id}")
-  status 200
-  return {:intent => setup_intent.id, :secret => setup_intent.client_secret}.to_json
-end
-
-# Looks up or creates a Customer on your stripe account
-# with email "example@test.com".
-def lookupOrCreateExampleCustomer
-  customerEmail = "example@test.com"
-  begin
-    customerList = Stripe::Customer.list(email: customerEmail, limit: 1).data
-    if (customerList.length == 1)
-      return customerList[0]
-    else
-      return Stripe::Customer.create(email: customerEmail)
-    end
-  rescue Stripe::StripeError => e
-    status 402
-    return log_info("Error creating or retreiving customer! #{e.message}")
-  end
-end
-
-# This endpoint attaches a PaymentMethod to a Customer.
-# https://stripe.com/docs/terminal/payments/saving-cards#read-reusable-card
-post '/attach_payment_method_to_customer' do
-  begin
-    customer = lookupOrCreateExampleCustomer
-
-    payment_method = Stripe::PaymentMethod.attach(
-      params[:payment_method_id],
-      {
-        customer: customer.id,
-        expand: ["customer"],
-    })
-  rescue Stripe::StripeError => e
-    status 402
-    return log_info("Error attaching PaymentMethod to Customer! #{e.message}")
-  end
-
-  log_info("Attached PaymentMethod to Customer: #{customer.id}")
-
-  status 200
-  # Note that returning the Stripe payment_method object directly creates a dependency between your
-  # backend's Stripe.api_version and your clients, making future upgrades more complicated.
-  # All clients must also be ready for backwards-compatible changes at any time:
-  # https://stripe.com/docs/upgrades#what-changes-does-stripe-consider-to-be-backwards-compatible
-  return payment_method.to_json
 end
 
 # This endpoint updates the PaymentIntent represented by 'payment_intent_id'.
